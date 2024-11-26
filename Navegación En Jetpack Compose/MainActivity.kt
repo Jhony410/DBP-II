a5898f0766dc4fb365b1f00d0fc670a5a6839ebb
Jhony Aguilar <130851818+Jhony410@users.noreply.github.com>
2024-11-26 16:16:55 -0500

    Add files via upload

---
+++ Navegación En Jetpack Compose/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.cursosant.retos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.cursosant.retos.core.navegation.NavigationWrapper
+import com.cursosant.retos.ui.theme.RetosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RetosTheme{
+                val navController = rememberNavController()
+                NavigationWrapper(navController)
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
